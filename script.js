74b007e69ab31699e7df07fe03acc90c58e68f23
E-Miteva <elena.miteva@student.pxl.be>
2026-05-08 12:58:53 +0200

    Homepagina aangepast en oude stukjes weggehaald

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+
+<script>
+    document.querySelectorAll('.hover-card').forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('open');
+        const text = card.querySelector('.card-hidden-text');
+        if (card.classList.contains('open')) {
+            text.style.maxHeight = text.scrollHeight + "px";
+            text.style.opacity = "1";
+        } else {
+            text.style.maxHeight = "0px";
+            text.style.opacity = "0";
         }
     });
 });
-
-document.querySelectorAll('.intro-section, .designer-section, .funfact-section, .footer-contact')
-    .forEach(section => observer.observe(section));
-
-
-
-
+</script>
 
